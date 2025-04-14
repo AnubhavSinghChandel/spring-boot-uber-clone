c9e41d5f809741898b086b91a42c6fc85d4ffd92
Anubhav Singh Chandel <anubhavsinghchandel1498@gmail.com>
2025-04-15 01:22:50 +0530

    updates

--- src/main/java/com/project/uber/uberApp/configs/WebSecurityConfig.java
+++ src/main/java/com/project/uber/uberApp/configs/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
 
     private final JWTAuthFilter jwtAuthFilter;
 
-    private final String[] PUBLIC_ROUTES = {"/auth/**"};
+    private final String[] PUBLIC_ROUTES = {"/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**"};
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
